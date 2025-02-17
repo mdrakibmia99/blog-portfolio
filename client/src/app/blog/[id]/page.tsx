b5587941f716477f39dd480a366855e3ae22f9a7
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-02-17 18:04:15 +0600

    update dynamic page metadata

--- client/src/app/blog/[id]/page.tsx
+++ client/src/app/blog/[id]/page.tsx
@@ -1,10 +1,24 @@
 // import BlogDetails from '@/components/blog/BlogDetails';
 import BlogDetails from '@/components/blog/BlogDetails';
+import { TBlog } from '@/types/globalTypes';
 import React from 'react';
 interface IProps {
     params: Promise<{
       id: string;
     }>;
+  }
+  export async function generateMetadata({ params }: IProps) {
+    const id = (await params)?.id;
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/blog/${id}`, {
+      cache: "no-store",
+    });
+    const data = await res.json();
+    const project: TBlog = data?.data;
+  
+    return {
+      title: project?.title,
+      description: project?.description,
+    };
   }
 const DynamicBlogDetailsPage = async({params}:IProps) => {
     const { id } = await params;
--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,7 +1,24 @@
 import ProjectDetails from '@/components/project/ProjectDetails';
 import { IParamsProps, TProject } from '@/types/globalTypes';
 import React from 'react';
-
+interface IProps {
+    params: Promise<{
+      id: string;
+    }>;
+  }
+export async function generateMetadata({ params }: IProps) {
+    const id = (await params)?.id;
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/project/${id}`, {
+      cache: "no-store",
+    });
+    const data = await res.json();
+    const project: TProject = data?.data;
+  
+    return {
+      title: project?.title,
+      description: project?.description,
+    };
+  }
 const DynamicProjectDetailsPage = async({params}:IParamsProps) => {
     const { id } = await params;
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/project/${id}`, {
